74e1008d9add5a99d351d6ea61bc0b46680fd0b9
Hamid V. Mammadov <Hamid.mammadov@code.local>
2022-04-28 18:06:48 +0400

    First Comit

---
+++ P224Allup/Models/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P224Allup.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [StringLength(255)]
+        public string FullName { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public IEnumerable<Basket> Baskets { get; set; }
+        public IEnumerable<Order> Orders { get; set; }
+    }
+}
